19a74cb886146a4c94f6c3b37e4819e8395c6596
Constanza Fontenla <mcf.constanza@gmail.com>
2023-11-07 14:44:43 -0300

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 
 //Indico que Carpeta es Publica
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 //Declaro el puerto por donde escucha el servidor
 let port = 3000
